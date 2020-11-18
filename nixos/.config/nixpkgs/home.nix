c203d386573ab9eccae622d746a3a272f05cbf78
beltsmith <me@alexgirdler.com>
2020-11-18 12:22:18 -0800

    home-manager

--- nixos/.config/nixpkgs/home.nix
+++ nixos/.config/nixpkgs/home.nix
@@ -2,8 +2,20 @@
 
 {
   home.packages = [
-    pkgs.glances
     pkgs.zplug
+    pkgs.glances
     pkgs.exa
   ];
+
+  programs.home-manager.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    autocd = true;
+  };
+
+  home.file = {
+    ".tmux.conf".source = ~/dotfiles/tmux/.tmux.conf;
+
+  };
 }
